56af32ec72e9b7518b2b5c65659feddd3ad515c7
jeffdong <dong.670@osu.edu>
2020-04-30 20:02:51 -0400

    firstcommit

---
+++ BookTests.cs
@@ -0,0 +1,45 @@
+using System;
+using Xunit;
+
+namespace Fisher.Bookstore.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+         public void AddBookPrice()
+        {
+            var book = new Book()
+            {
+                Id=1,
+                Title = "My Test Book",
+                Price = 9.99
+            };
+            
+            //Act   
+            book.Price = 7.99;
+
+            //Assert
+            Assert.Equal(7.99, book.Price);
+
+        }
+        public void TestMemberDiscount()
+        {
+              var book = new Book()
+            {
+                Id = 1,
+                Title = "My Test Book",
+                Price = 9.99
+            };
+
+
+            book.ApplyMemberDiscount();
+
+            var discountPrice = 9.99 * .9;
+            Assert.Equal(discountPrice, book.Price);
+
+
+
+        }
+        
+    }
+}
